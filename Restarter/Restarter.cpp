a67f541481ef140c26fb7206a30f72a6af7e13ab
Krzysztof Grochocki <Krzysztof Grochocki@localhost>
2014-04-20 23:02:00 +0200

    * Dodanie 64-bitowej wersji wtyczki. * Autologowanie na profile niezabezpieczone has?em bez opcji autologowania. * Zmiany w pliku wspomagaj?cym restart. * Inne mniej wa?ne zmiany w kodzie wtyczki.

--- Restarter/Restarter.cpp
+++ Restarter/Restarter.cpp
@@ -1,34 +1,66 @@
-//---------------------------------------------------------------------------
-#include <vcl.h>
 #pragma hdrstop
+#pragma argsused
 #include <tchar.h>
+#include <windows.h>
 //---------------------------------------------------------------------------
-USEFORM("RestarterFrm.cpp", RestarterForm);
-//---------------------------------------------------------------------------
-int WINAPI _tWinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
+
+//Interwal wylaczenie programu
+int GiveUpCount = 0;
+
+int _tmain(int argc, _TCHAR* argv[])
 {
-	try
+  //Hook dla petli
+  START:
+  //Ukrywanie chmurki informacyjnej
+  HWND hWindowHandle = FindWindow(L"TfrmMiniStatus",NULL);
+  if(hWindowHandle) ShowWindow(hWindowHandle, SW_HIDE);
+  //Szukanie okna logowania
+  hWindowHandle = FindWindow(L"TfrmLogon",NULL);
+  //Jezeli okno zostalo znalezione
+  if((hWindowHandle)&&(IsWindow(hWindowHandle))&&(IsWindowVisible(hWindowHandle)))
+  {
+	//Szukanie kontrolki przycisku OK
+	hWindowHandle = FindWindowEx(hWindowHandle,NULL,L"TsButton",L"OK");
+	//Kontrolka zostala znaleziona
+	if(hWindowHandle)
 	{
-		Application->Initialize();
-		Application->MainFormOnTaskBar = true;
-		Application->CreateForm(__classid(TRestarterForm), &RestarterForm);
-		Application->Run();
+	  //Wysylanie emulacji klikniecia
+	  SendMessage(hWindowHandle, BM_CLICK, 0, 0);
+	  //Zamkniecie programu
+	  return 0;
 	}
-	catch (Exception &exception)
+	//Kontrolka nie zostala znaleziona
+	else
 	{
-		Application->ShowException(&exception);
+	  //Interwal wylaczenie programu
+	  GiveUpCount++;
+	  //Interwal wylaczenia programu zostal osiagniety
+	  if(GiveUpCount==200)
+	   return 0;
+	  //Uspienie programu na 100ms
+	  else
+	  {
+		Sleep(100);
+		goto START;
+	  }
 	}
-	catch (...)
+  }
+  //Okno nie zostalo znalezione
+  else
+  {
+	//Interwal wylaczenie programu
+	GiveUpCount++;
+	//Interwal wylaczenia programu zostal osiagniety
+	if(GiveUpCount==200)
+	 return 0;
+	//Uspienie programu na 100ms
+	else
 	{
-		try
-		{
-			throw Exception("");
-		}
-		catch (Exception &exception)
-		{
-			Application->ShowException(&exception);
-		}
+	  Sleep(100);
+	  goto START;
 	}
-	return 0;
+  }
+
+  return 0;
 }
 //---------------------------------------------------------------------------
